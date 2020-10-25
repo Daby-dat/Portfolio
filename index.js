4c7bf503755310f5a7466fffed91a6a3eb09b9d5
David Gómez <alejandrogomez2510@gmail.com>
2020-10-25 03:40:29 -0400

    Last changes

--- index.js
+++
@@ -1,6 +0,0 @@
-var http = require('http')
-
-http.createServer(function (request,response) {
-    response.writeHead(200,{"Content-Type": "text/plain"})
-    response.end("Build Finished\n")
-}).listen(process.env.PORT)
