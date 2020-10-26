4f29829b90967b2fac8ddcaa3e01c7df0b6e1f42
David Gómez <alejandrogomez2510@gmail.com>
2020-10-26 14:11:02 -0400

    Updating

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,19 +2,15 @@ $(window).scroll(function () {
     $('nav').toggleClass('scrolled', $(this).scrollTop() > 150);
 });
 
-$("#firstlink").venobox().trigger('click');
 
-$('.venobox').venobox({
-    framewidth : '400px',                            // default: ''
-    frameheight: '300px',                            // default: ''
-    border     : '10px',                             // default: '0'
-    bgcolor    : '#5dff5e',                          // default: '#fff'
-    titleattr  : 'data-title',                       // default: 'title'
-    numeratio  : true,                               // default: false
-    infinigall : true,                               // default: false
-    share      : ['facebook', 'twitter', 'download'] // default: []
+$(document).ready(function () {
+    $('.venobox').venobox({
+        spinner: 'rotating-plane',
+        spinColor: '#DCD31D',
+    });
 });
 
+$("#firstlink").venobox().trigger('click');
 
 
 $(".carousel").on("mouseenter", function () {
