f8a940b79ccb5d5ce7859389a3cc4a86217b2517
David Gómez <alejandrogomez2510@gmail.com>
2020-11-22 11:55:16 -0400

    Update

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,12 +18,11 @@ $(".carousel").on("mouseenter", function () {
     $(this).carousel('pause');
 });
 
-
 $('nav a').click(function(e){				
-		e.preventDefault();		//evitar el eventos del enlace normal
-		var strAncla=$(this).attr('href'); //id del ancla
-			$('body,html').stop(true,true).animate({				
-				scrollTop: $(strAncla).offset().top
-			},0);
-		
-	});
+    e.preventDefault();		//evitar el eventos del enlace normal
+    var strAncla=$(this).attr('href'); //id del ancla
+        $('body,html').stop(true,true).animate({				
+            scrollTop: $(strAncla).offset().top
+        },100);
+    
+});
